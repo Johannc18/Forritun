decafc60f68dfb6edd3157f630af2a8c31ce81e1
unknown <johannc18@ru.is>
2018-09-04 11:53:57 +0000

    Algorithm written for sequence

--- max_int.py
+++ max_int.py
@@ -10,7 +10,7 @@ num_int = int(input("Input a number: "))    # Do not change this line
 #else print max_int
 
 max_int = 0
-while num_int > -1:
+while num_int >= 0:
     if num_int > max_int:
         max_int = num_int
         num_int = int(input("Input a number: "))
---
+++ sequence.py
@@ -0,0 +1,10 @@
+n = int(input("Enter the length of the sequence: ")) # Do not change this line
+
+# insert number n 
+# assign x = 0
+# while loop for x < n
+# define a b c to 0 1 0
+# create trib variable that is sum of a b c
+# assign a,b,c to trib, b, c
+# increment x by 1
+# print each number successively
